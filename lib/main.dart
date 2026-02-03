68133920e68595a63fd6dee4f989755e0f5c1799
BiBO <iqbalfadila97@gmail.com>
2026-02-03 13:58:41 +0700

    Implement login and splash screens with custom components and asset management

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ticketing_apps/core/constants/colors.dart';
 import 'package:ticketing_apps/ui/auth/splash_screen.dart';
 
 void main() {
@@ -12,8 +14,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: AppColors.primary),
+        dialogTheme: DialogThemeData(elevation: 0),
+        textTheme: GoogleFonts.plusJakartaSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0,
+          titleTextStyle: GoogleFonts.plusJakartaSans(
+            color: AppColors.primary,
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+          ),
+          iconTheme: IconThemeData(color: AppColors.black),
+          centerTitle: true,
+        ),
+      ),
       home: SplashScreen(),
     );
   }
--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -1,15 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:ticketing_apps/core/assets/assets.gen.dart';
+import 'package:ticketing_apps/core/components/button.dart';
+import 'package:ticketing_apps/core/components/custom_text_field.dart';
+import 'package:ticketing_apps/core/components/spaces.dart';
+import 'package:ticketing_apps/core/constants/colors.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Login Screen'),
+      backgroundColor: AppColors.primary,
+      body: Stack(
+        children: [
+          //bagian 1
+          SizedBox(
+            height: 200,
+            child: Center(child: Assets.images.logoWhite.image(height: 55)),
+          ),
+
+          //bagian 2
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SingleChildScrollView(
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.vertical(
+                  top: Radius.circular(20),
+                ),
+                child: ColoredBox(
+                  color: AppColors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 44,
+                      horizontal: 28,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CustomTextField(
+                          label: 'Email',
+                          controller: emailController,
+                          isOutlineBorder: false,
+                        ),
+                        SpaceHeight(36),
+                        CustomTextField(
+                          controller: passwordController,
+                          label: 'Password',
+                          isOutlineBorder: false,
+                        ),
+                        SpaceHeight(36),
+                        Button.filled(onPressed: () {}, label: 'Login'),
+                        SpaceHeight(120),
+                        Center(child: Assets.images.logo.image(height: 88)),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
