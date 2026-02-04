b9a7c8c72f9aad2843069a68d76a71c3013ae965
BiBO <iqbalfadila97@gmail.com>
2026-02-04 15:12:30 +0700

    Remove macOS project files and configurations, including Xcode schemes, assets, and entitlements. Add currency formatting extension and product model for ticketing application.

---
+++ lib/ui/home/model/product_model.dart
@@ -0,0 +1,55 @@
+class ProductModel {
+  final String productName;
+  final String type;
+  final int price;
+  int quantity;
+
+  ProductModel({
+    required this.productName,
+    required this.type,
+    required this.price,
+    this.quantity = 1,
+  });
+}
+
+final dummyProducts = [
+  ProductModel(
+    productName: 'Ticket Dewasa',
+    type: 'Domestik',
+    price: 15000),
+  ProductModel(
+    productName: 'Ticket Anak Kecil',
+    type: 'Domestik',
+    price: 10000,
+  ),
+  ProductModel(
+    productName: 'Ticket Dewasa',
+    type: 'Manca Negara',
+    price: 150000,
+  ),
+  ProductModel(
+    productName: 'Ticket Bocil',
+    type: 'Manca Negara',
+    price: 100000,
+  ),
+  ProductModel(
+    productName: 'Ticket Kematian',
+    type: 'Manca Negara',
+    price: 100000,
+  ),
+  ProductModel(
+    productName: 'Ticket Bocil',
+    type: 'Manca Negara',
+    price: 100000,
+  ),
+  ProductModel(
+    productName: 'Ticket Kehidupan',
+    type: 'Manca Negara',
+    price: 100000,
+  ),
+  ProductModel(
+    productName: 'Ticket Anak Dewasa',
+    type: 'Manca Negara',
+    price: 100000,
+  ),
+];
