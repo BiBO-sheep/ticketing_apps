332282a9ca670d6c0d049ef10ac976c439790c90
BiBO <iqbalfadila97@gmail.com>
2026-02-10 15:22:31 +0700

    Implement settings screen with logout and sync data dialogs, and add flutter_spinkit dependency

---
+++ lib/ui/dialog/sync_data_dialog.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'package:ticketing_apps/core/components/spaces.dart';
+import 'package:ticketing_apps/core/constants/colors.dart';
+
+class SyncDataDialog extends StatefulWidget {
+  const SyncDataDialog({super.key});
+
+  @override
+  State<SyncDataDialog> createState() => _SyncDataDialogState();
+}
+
+class _SyncDataDialogState extends State<SyncDataDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return const AlertDialog(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SpaceHeight(40),
+          SpinKitCubeGrid(color: AppColors.primary, size: 80),
+          SpaceHeight(16),
+          Text(
+            'Sync Data',
+            style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
