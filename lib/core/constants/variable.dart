8564b5839a6a4f0d2288a3364ac30a0d5da786e2
BiBO <iqbalfadila97@gmail.com>
2026-02-13 13:02:02 +0700

    Add authentication models and update dependencies for API integration

---
+++ lib/core/constants/variable.dart
@@ -0,0 +1,3 @@
+class Variable {
+  static const String baseUrl = 'https://api-ticketing.bngshot.my.id/';
+}
---
+++ lib/core/data/datasources/auth_remote_datasources.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
+import 'package:ticketing_apps/core/constants/variable.dart';
+import 'package:ticketing_apps/models/requests/login_request_model.dart';
+import 'package:ticketing_apps/models/responses/login_response_model.dart';
+
+class AuthRemoteDatasource {
+  Future<Either<String, LoginResponseModel>> login(
+    LoginRequestModel data,
+  ) async {
+    final response = await http.post(
+      Uri.parse('${Variable.baseUrl}/api/login'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: data.toJson(),
+    );
+
+    if (response.statusCode == 200) {
+      return Right(LoginResponseModel.fromJson(response.body));
+    } else {
+      return left(response.body);
+    }
+  }
+}
